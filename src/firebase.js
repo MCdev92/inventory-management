82ae9e3b15923ba546099780766e8104095550c7
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2024-08-01 21:02:10 -0700

    relocated the firebase file and set up database system.

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_u6VzmlfP-dO5ZN4LrVvSLjpL7aNuJFE",
+    authDomain: "inventory-management-fa4d3.firebaseapp.com",
+    projectId: "inventory-management-fa4d3",
+    storageBucket: "inventory-management-fa4d3.appspot.com",
+    messagingSenderId: "559938537554",
+    appId: "1:559938537554:web:038460cb4df76ccd59b78b",
+    measurementId: "G-XM2JS4YSS3"
+  };
+  
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
