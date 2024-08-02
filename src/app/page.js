f9a081fc4a2903e002020e3a177fcf440c4f9f9f
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2024-08-02 07:25:30 -0700

    change app name

--- src/app/page.js
+++ src/app/page.js
@@ -160,7 +160,7 @@ export default function Home() {
           alignItems="center"
         >
           <Typography variant="h2" color="#333" textAlign="center">
-            Inventory Items
+            Pantry Tracker
           </Typography>
         </Box>
         <Stack width="800px" height="300px" spacing={2} overflow="auto">
